81c210e07e79df2a2b39098fcfa41bd6000c98de
KEN1965 <hammer528@msn.com>
2025-10-22 17:35:44 +0900

    Initial Commit

---
+++ ShortcutHUD/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShortcutHUD
+//
+//  Created by Kenichi on R 7/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShortcutHUD/ShortcutHUDApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShortcutHUDApp.swift
+//  ShortcutHUD
+//
+//  Created by Kenichi on R 7/10/22.
+//
+
+import SwiftUI
+
+@main
+struct ShortcutHUDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
